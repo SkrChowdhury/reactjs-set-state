2e28a2892756d4006ad7eef66fd927dd2cd67cd0
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-20 00:27:43 +0600

    state , set state, state hook in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
 
+function Counter() {
+  const [count, setCount] = useState(0); //0 initial value
+  const increaseCount = () => {
+    // const newCount = count + 1;
+    // setCount(newCount);
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <h1>Count:{count} </h1>
+      <button onClick={decreaseCount}>Decrease</button>
+      <button onClick={increaseCount}>Increase</button>
     </div>
   );
 }
